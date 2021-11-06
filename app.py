451698ac9fd8407cad36e72c622e40bb1fe1a3d2
Paul Warta <paul@team-warta.de>
2021-11-06 12:57:08 +0100

    u

--- app.py
+++ app.py
@@ -18,8 +18,9 @@ lehrerAbkürzungen = {
 }
 
 
+# Laedt die Dateien aus dem Stundenplan (von Patrick, aber ausschließlich die Veranstaltungen die nicht in der Nimbuscloud eingetragen sind) und wandelt diese in das von uns verwendete Schema um
 def Stundenplan ():
-    response = requests.post('https://anmeldung.tanzhaus-muelheim.de/saalbelegung/', data={'apikey':sys.argv[1]})
+    response = requests.post('https://anmeldung.tanzhaus-muelheim.de/saalbelegung/', data={'apikey':'db7a5918390d7c70d3ea9bc02afa667c3147097664ad5e9ee4a772f42c7259da'})
 
     response = response.text
     response = json.loads(response)
@@ -57,11 +58,12 @@ def Stundenplan ():
         response = ""
         return response
     
+# Laedt die Dateien aus der Nimuscloud und wandelt diese in das von uns verwendete Schema um
 def Nimbuscloud ():
     current_date = datetime.now() - timedelta(days=1)
     timestamp = int(current_date.timestamp())
     
-    response = requests.post("https://tanzschule-ritter.nimbuscloud.at/api/json/v1/timetable/data", data={"apikey":sys.argv[2], "date":timestamp, "days": "2", "programOnlyNew": "false"})
+    response = requests.post("https://tanzschule-ritter.nimbuscloud.at/api/json/v1/timetable/data", data={"apikey":'db7a5918390d7c70d3ea9bc02afa667c3147097664ad5e9ee4a772f42c7259da', "date":timestamp, "days": "2", "programOnlyNew": "false"})
     response = response.text
     response = json.loads(response)
     response = response["content"]
@@ -85,23 +87,29 @@ def Nimbuscloud ():
 nimbusdata = Nimbuscloud()
 stundenplandata = Stundenplan()
 
+#Verbindet die beiden Arrays in ein großes
 stundenplandata = nimbusdata + stundenplandata
 stundenplandata = stundenplandata.replace('][', ', ')
 stundenplandata = json.loads(stundenplandata)
 
-start = {}
 
+#sortiert die Veranstaltungen nach Zeit (nötig, da sonst die Daten des Stundenplans vor (oder nach) denen der Nimbuscloud einsortiert sind)
+start = {}
 for i in range(len(stundenplandata)):
     start[i] = stundenplandata[i]["Von"].replace(':', '')
-
-start = sorted(start.items(), key=lambda x: x[1]) 
-
+start = sorted(start.items(), key=lambda x: x[1])
 stundenplandataNew = []
-
 for i in range(len(start)):
     stundenplandataNew.append(stundenplandata[start[i][0]])
 
 stundenplandata = stundenplandataNew
+
+# Diese Arrays sind zum testen der Deuplikate da: 
+# saal1 = [{"Von": "09:00", "Bis": "10:30", "Saal": 1, "Bem": "Formationen", "Lehrer": ""}, {"Von": "09:00", "Bis": "10:30", "Saal": 1, "Bem": "Formationen 2", "Lehrer": ""}, {"Von": "10:30", "Bis": "11:30", "Saal": 1, "Bem": "Formationen", "Lehrer": ""}, {"Von": "12:00", "Bis": "13:30", "Saal": 1, "Bem": "Formationen", "Lehrer": "Coach Ryu"}]
+# saal2 = [{"Von": "10:30", "Bis": "12:00", "Saal": 2, "Bem": "Formationen", "Lehrer": ""}]
+# saal3 = []
+# saal4 = []
+# saal5 = [{"Von": "12:00", "Bis": "13:30", "Saal": 5, "Bem": "Formationen", "Lehrer": "Linda Ritter"}, {"Von": "13:30", "Bis": "15:00", "Saal": 5, "Bem": "Formationen", "Lehrer": "Coach Ryu"}, {"Von": "15:00", "Bis": "16:30", "Saal": 5, "Bem": "Formationen", "Lehrer": "Coach Ryu"}]
 
 saal1 = []
 saal2 = []
@@ -109,6 +117,7 @@ saal3 = []
 saal4 = []
 saal5 = []
 
+# Sortiert aus dem Hauptarray die Veranstaltungen in die Saele
 for i in range(len(stundenplandata)):
     if stundenplandata[i]["Saal"] == 1:
         saal1.append(stundenplandata[i])
@@ -121,6 +130,16 @@ for i in range(len(stundenplandata)):
     if stundenplandata[i]["Saal"] == 5:
         saal5.append(stundenplandata[i])
 
+# Die nachfolgende Schleife kontrolliert die Arrays der Saele auf eine gleiche Startuhrzeit wobei die Elemente verschieden sein müssen und löscht ggf. das Duplikat
+for i in range(1, 6):
+    saal = eval("saal" + str(i))
+    for vergleichsElement in saal:
+        for element in saal:
+            if (vergleichsElement["Von"] == element["Von"] and str(vergleichsElement) != str(element)):
+                saal.remove(element)
+            
+
+    
 saal1 = json.dumps(saal1)
 saal2 = json.dumps(saal2)
 saal3 = json.dumps(saal3)
@@ -134,6 +153,7 @@ print(saal4)
 print(saal5)
 
 
+# Schreibt in die Files für das UI
 
 f = open("stundenplan1.json", "w")
 f.write(saal1)
